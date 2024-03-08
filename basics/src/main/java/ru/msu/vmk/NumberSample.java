0acb448bb3707ae7b401d9ed935416523b74e24c
ElenaPolitov001 <93623352+ElenaPolitov001@users.noreply.github.com>
2024-03-09 00:40:46 +0300

    Update NumberSample.java
    Zadanie 1

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -3,9 +3,22 @@ package ru.msu.vmk;
 import java.math.BigDecimal;
 
 public class NumberSample {
+
+    public static void main(String[] args) {
+        // вызов split - проверка работы
+    }
+
     public static BigDecimal[] split(BigDecimal amount, int n) {
-        // разделить amount на n частей
-        // если остается остаток, прибавить его к первому числу
-        return null;
+        BigDecimal[] result = new BigDecimal[n];
+        BigDecimal splitAmount = amount.divide(new BigDecimal(n), 2, BigDecimal.ROUND_HALF_EVEN);
+        BigDecimal remainder = amount.remainder(new BigDecimal(n));
+
+        for (int i = 0; i < n; i++) {
+            result[i] = splitAmount;
+        }
+        
+        result[0] = result[0].add(remainder);
+
+        return result;
     }
 }
